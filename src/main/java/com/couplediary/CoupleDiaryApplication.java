4a728e342d1e83f3224df6b7220648d21931977f
jyoooooung <jyoooooung@naver.com>
2024-01-13 19:10:57 +0900

    couple-diary start

---
+++ src/main/java/com/couplediary/CoupleDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.couplediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupleDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupleDiaryApplication.class, args);
+	}
+
+}
