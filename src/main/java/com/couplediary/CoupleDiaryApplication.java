d57a71826b01791d53ca59faa06c5b2cf6e00b20
ohjinhokor <ohjinho7@gmail.com>
2024-04-09 20:30:45 +0900

    feat: health-check api 추가

--- src/main/java/com/couplediary/CoupleDiaryApplication.java
+++ src/main/java/com/couplediary/CoupleDiaryApplication.java
@@ -3,10 +3,23 @@ package com.couplediary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CoupleDiaryApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CoupleDiaryApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CoupleDiaryApplication.class, args);
+	}
+}
+
+@RestController
+@RequestMapping("/health-check")
+class HealthCheckController {
+	@GetMapping
+	public ResponseEntity<String> healthCheck() {
+		return ResponseEntity.ok().body("ok");
+	}
 }
